ce037d919e71d2b8bc5600fb6d29ad6ab538a65c
Hanmengtong <719084437@qq.com>
2021-04-08 00:10:29 +0800

    处理登录错误、用户注册

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -9,17 +9,19 @@
             <nuxt-link v-if="isLogin" to="/register">Need an account?</nuxt-link>
           </p>
           <ul class="error-messages">
-            <li>That email is already taken</li>
+            <template v-for="(msgs, field) in errors">
+              <li v-for="(msg, index) in msgs" :key="index">{{field}} {{msg}}</li>
+            </template>
           </ul>
           <form @submit.prevent="onSubmit">
             <fieldset class="form-group" v-if="!isLogin">
-              <input class="form-control form-control-lg" type="text" placeholder="Your Name" required/>
+              <input class="form-control form-control-lg" type="text" placeholder="Your Name" v-model="user.username" required/>
             </fieldset>
             <fieldset class="form-group">
               <input class="form-control form-control-lg" type="email" placeholder="Email" v-model="user.email" required/>
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="password" placeholder="Password" v-model="user.password" required/>
+              <input class="form-control form-control-lg" type="password" placeholder="Password" v-model="user.password" required minlength="8"/>
             </fieldset>
             <button class="btn btn-lg btn-primary pull-xs-right">{{isLogin ? 'Sign in' : 'Sign up'}}</button>
           </form>
@@ -30,27 +32,37 @@
 </template>
 
 <script>
-import { login } from '@/api/user'
+import { login, register } from '@/api/user'
 export default {
     name: 'LoginIndex',
     data() {
       return {
         user: {
+          username: '',
           email: '',
           password: ''
-        }
+        },
+        errors: {} // 错误信息
       }
     },
     methods: {
       async onSubmit () {
-        // 提交表单登陆
-        const { data } = await login({
-          user: this.user
-        })
-        console.log(data)
-        // 保存用户的登录状态
-        // 跳转至首页
-        this.$router.push('/')
+        try {
+          // 提交表单登陆
+          const { data } = this.isLogin ? await login({
+            user: this.user
+          }) : await register({
+            user: this.user
+          })
+          console.log(data)
+          // 保存用户的登录状态
+          
+          // 跳转至首页
+          this.$router.push('/')
+        } catch (error) {
+          // console.log('请求失败', error)
+          this.errors = error.response.data.errors
+        }
       }
     },
     computed: {
