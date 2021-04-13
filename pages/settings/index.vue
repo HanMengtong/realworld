ebd015daa7572eee3e11a042dbdae8ffeee518c9
Hanmengtong <719084437@qq.com>
2021-04-14 01:08:17 +0800

    获取当前用户信息

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,44 +1,60 @@
 <template>
-    <div class="settings-page">
-  <div class="container page">
-    <div class="row">
-
-      <div class="col-md-6 offset-md-3 col-xs-12">
-        <h1 class="text-xs-center">Your Settings</h1>
-
-        <form>
-          <fieldset>
+  <div class="settings-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Your Settings</h1>
+          <form>
+            <fieldset>
               <fieldset class="form-group">
-                <input class="form-control" type="text" placeholder="URL of profile picture">
+                <input class="form-control" type="text" placeholder="URL of profile picture" v-model="user.image">
               </fieldset>
               <fieldset class="form-group">
-                <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+                <input class="form-control form-control-lg" type="text" placeholder="Your Name" v-model="user.username">
               </fieldset>
               <fieldset class="form-group">
-                <textarea class="form-control form-control-lg" rows="8" placeholder="Short bio about you"></textarea>
+                <textarea class="form-control form-control-lg" rows="8" placeholder="Short bio about you" v-model="user.bio"></textarea>
               </fieldset>
               <fieldset class="form-group">
-                <input class="form-control form-control-lg" type="text" placeholder="Email">
+                <input class="form-control form-control-lg" type="text" placeholder="Email" v-model="user.email">
               </fieldset>
               <fieldset class="form-group">
-                <input class="form-control form-control-lg" type="password" placeholder="Password">
+                <input class="form-control form-control-lg" type="password" placeholder="New Password" v-model="user.password" required>
               </fieldset>
-              <button class="btn btn-lg btn-primary pull-xs-right">
+              <button class="btn btn-lg btn-primary pull-xs-right" @click="updateClick">
                 Update Settings
               </button>
           </fieldset>
         </form>
       </div>
-
     </div>
   </div>
 </div>
 </template>
 
 <script>
+import { getUser, updateUser } from '@/api/user'
 export default {
   middleware: 'auth',
-  name: 'Settings'
+  name: 'Settings',
+  async asyncData({ params }) {
+    const { data } = await getUser()
+    const { user } = data
+    user.password = ''
+    return {
+      user
+    }
+  },
+  methods: {
+    async updateClick () {
+      const { data } = await updateUser(this.user)
+      // 保存用户的登录状态
+      this.$store.commit('setUser', data.user)
+      // 为了防止刷新页面数据丢失，需要将数据持久化
+      cookie.set('user', data.user)
+      this.$router.push({name: 'Profile', params: {username: data.username}})
+    }
+  }
 }
 </script>
 
